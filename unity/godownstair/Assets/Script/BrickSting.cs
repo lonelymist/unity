a1581230347da239498e0dc477a692108be0442d
becomerain <46097683+becomerain@users.noreply.github.com>
2019-05-08 22:05:13 +0800

    bricksting hi

--- unity/godownstair/Assets/Script/BrickSting.cs
+++ unity/godownstair/Assets/Script/BrickSting.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BrickSting : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("hi");
+    }
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
